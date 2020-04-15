9c755b16541a125ed7fd199e1ca89a2f7c447400
fabezio <fabezio@outlook.fr>
2020-04-15 21:35:07 +0200

    ajout du fichier variables

---
+++ variables.py
@@ -0,0 +1,25 @@
+#coding:utf-8
+# ci-dessus une instructin d'encodage. ici une ligne de commentaire
+'''
+là un comm sur 
+plusieurs lignes
+'''
+"""
+ça marche avec des séries de 3 apostrophes ou guillemets de part et d'autre du commentaire
+"""
+"""
+print("Salut tout le monde! \nC'est bon dêtre là!")
+# variables
+prenom = "Fabezio"
+nom = "RICCI"
+age = 46
+agePlusUn = age + 1
+presentation = "Salut! Je m'appelle {} et j'ai {} ans ({} bientôt.)"
+print(presentation.format(prenom, age, age+1))
+print(type(nom))
+nom = 102
+print(type(nom))
+"""
+nomDuJoueur = input('Quel est tom nom?')
+score = 0
+print(nomDuJoueur, score+1)
